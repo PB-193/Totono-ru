a61dee5928178e71323d6eca95a58821153661e6
pb-193 <bigbamboopbo@yahoo.co.jp>
2023-06-14 15:02:53 +0000

    レイアウトを調整

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,4 +1,5 @@
 class User::SearchesController < ApplicationController
+  
   def find
     # 検索フォームを表示するアクション
   end
@@ -6,10 +7,15 @@ class User::SearchesController < ApplicationController
   def index
     start_year = params[:start_year]
     end_year = params[:end_year]
-
+  
     # start_year から end_year の範囲でコンテンツを絞り込む
     @contents = Content.where(visit_day: start_year..end_year)
-
-    # 検索結果を表示するアクション
+  
+    if params[:tag_id].present?
+      @tag = Tag.find(params[:tag_id])
+      @contents = @contents.joins(:tags).where(tags: { id: @tag.id })
+    end
   end
+
+  # 検索結果を表示するアクション
 end
