244a57364a0cd22420708b2732f913e072c114bc
pb-193 <bigbamboopbo@yahoo.co.jp>
2023-06-21 10:44:35 +0000

    検索の修正

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -5,8 +5,30 @@ class User::SearchesController < ApplicationController
   end
 
   def index
-    @tag = Tag.find(params[:tag_id])
-    @contents = @tag.contents
+    @tag = Tag.find_by(id: params[:tag_id])
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+  
+    # 絞り込み条件に基づいてデータを取得するクエリを作成
+    query = Content.all
+  
+    if @start_date.present?
+      query = query.where("created_at >= ?", @start_date)
+    end
+  
+    if @end_date.present?
+      query = query.where("created_at <= ?", @end_date)
+    end
+  
+    # タグが指定されている場合はタグに基づいて絞り込み
+    if @tag.present?
+      query = query.joins(:tags).where(tags: { id: @tag.id })
+    end
+  
+    # クエリを実行して結果を取得
+    @contents = query.order(created_at: :desc)
+    render "user/searches/index"
   end
+
 
 end
