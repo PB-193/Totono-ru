d9daa6709f747e459b02308357270933da93d144
pb-193 <bigbamboopbo@yahoo.co.jp>
2023-07-09 06:24:47 +0000

    いいね機能の実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,21 @@ class Comment < ApplicationRecord
     
     validates :comment,presence:true
 
+    # コメント日時の表示
+    def how_long_ago
+      if (Time.now - self.created_at) <= 60 * 60
+        # 60分以内なら
+        ((Time.now - self.created_at) / 60).to_i.to_s + "分前"
+      elsif (Time.now - self.created_at) <= 60 * 60 * 24
+        # 24時間以内なら
+        ((Time.now - self.created_at) / 3600).to_i.to_s + "時間前"
+      elsif (Date.today - self.created_at.to_date) <= 30
+        # 30日以内なら
+        (Date.today - self.created_at.to_date).to_i.to_s + "日前"
+      else
+        # それ以降
+        self.created_at
+      end
+    end
+
 end
