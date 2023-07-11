6d723cc30c20248ab0b30e990c9d605dc256f04a
pb-193 <bigbamboopbo@yahoo.co.jp>
2023-07-11 07:32:58 +0000

    いいね機能を非同期通信化

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,17 +1,15 @@
 class User::FavoritesController < ApplicationController
   
   def create
-    content = Content.find(params[:content_id])
-    favorite = current_user.favorites.new(content_id: content.id)
+    @content = Content.find(params[:content_id])
+    favorite = current_user.favorites.new(content_id: @content.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    content = Content.find(params[:content_id])
-    favorite = current_user.favorites.find_by(content_id: content.id)
+    @content = Content.find(params[:content_id])
+    favorite = current_user.favorites.find_by(content_id: @content.id)
     favorite.destroy
-    redirect_to request.referer
   end
   
 end
