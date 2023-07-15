f8b85c0ce0022df4a6de729b5cb6a5e58e03d4bb
pb-193 <bigbamboopbo@yahoo.co.jp>
2023-07-15 09:14:48 +0000

    ゲストログイン中のいいねを制限しました

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,4 +1,5 @@
 class User::FavoritesController < ApplicationController
+  before_action :guest_check, only: [:new, :create]
   
   def create
     @content = Content.find(params[:content_id])
@@ -10,6 +11,12 @@ class User::FavoritesController < ApplicationController
     @content = Content.find(params[:content_id])
     favorite = current_user.favorites.find_by(content_id: @content.id)
     favorite.destroy
+  end
+  
+  def guest_check
+    if current_user.email == 'guest@example.com'
+      redirect_to request.referer, alert: "いいねをするにはユーザ登録が必要です。"
+    end
   end
   
 end
